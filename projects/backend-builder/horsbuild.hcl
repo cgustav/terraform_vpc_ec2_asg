b12d16bcb37657895858bb0d62e4bcf695ba98fb
Eduardo Munoz <gstv.qnd@outlook.com>
2024-06-30 05:01:03 -0400

    🐛 Add pokemons servers port 22

--- projects/backend-builder/horsbuild.hcl
+++
@@ -1,5 +0,0 @@
-bucket         = "pokemon-app-terraform-bucket-state-store"
-key            = "terraform.tfstate"
-region         = "us-east-1"
-dynamodb_table = "pokemon-app-terraform-state-lock"
-encrypt        = false
--- projects/pokemon_game/modules/security/main.tf
+++ projects/pokemon_game/modules/security/main.tf
@@ -36,6 +36,14 @@ resource "aws_security_group" "web" {
   description = "Security group for web servers"
   vpc_id      = var.vpc_id
 
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port       = 80
     to_port         = 80
